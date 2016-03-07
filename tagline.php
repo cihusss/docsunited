48fd42d1b1ee74b3f1ff1ce43df75721d6e215cc
cihus <cihusss@gmail.com>
2016-03-07 14:53:29 -0500

    changed the tagline

--- tagline.php
+++ tagline.php
@@ -1,5 +1,5 @@
 <div class="row">
 	<div class="col-1-1 center-block-sm bg-blue-vivid pad-md col-flex-row">
-		<p class="fg-white">Filling the needs of both facilities and physicians with the right fit, in the right place, at the right time.</p>
+		<p class="fg-white">Fulfilling the needs of both facilities and physicians with the right fit, in the right place, at the right time.</p>
 	</div>
 </div>
